2d52ebc2494aa193175b346038e2080907917f6d
Spacecoder <aleks-bobr07@live.com>
2021-07-07 22:21:03 +0300

    Complete

---
+++ MVVM_SimpleApp/MVVM_SimpleApp/Model/User.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using MVVM_SimpleApp.ViewModel;
+
+namespace MVVM_SimpleApp.Model
+{
+    [Serializable]
+    public class User : ViewModelBase
+    {
+        private Guid _id;
+        [XmlAttribute("ID")]
+        public Guid ID
+        {
+            get { return _id; }
+            set 
+            {
+                _id = value;
+                OnPropertyChanged("ID");
+            }
+        }
+
+        private string _name;
+        [XmlElement("Name")]
+        public string Name
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string _surname;
+        [XmlElement("Surname")]
+        public string Surname
+        {
+            get { return _surname; }
+            set 
+            { 
+                _surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+
+
+        private int _age;
+        [XmlElement("Age")]
+        public int Age
+        {
+            get { return _age; }
+            set 
+            { 
+                _age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        private string _role;
+        [XmlElement("Role")]
+        public string Role
+        {
+            get { return _role; }
+            set { 
+                _role = value;
+                OnPropertyChanged("Role");
+            }
+        }
+
+    }
+}
---
+++ MVVM_SimpleApp/MVVM_SimpleApp/ViewModel/UsersViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Serialization;
+using MVVM_SimpleApp.Model;
+
+
+namespace MVVM_SimpleApp.ViewModel
+{
+    class UsersViewModel : ViewModelBase
+    {
+        public ObservableCollection<User> Users { get; set; }
+        private XmlRootAttribute rootAttribute = new XmlRootAttribute("Users");
+        public UsersViewModel()
+        {
+            if (File.Exists("users.xml"))
+            {
+                LoadFile();
+            }
+        }
+
+        private User _selectedUser;
+
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set 
+            { 
+                _selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+        private RelayCommand saveCommand;
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                return saveCommand ??
+                    (saveCommand = new RelayCommand(obj =>
+                    {
+                        SaveFile();
+                    }));
+            }
+        }
+
+        private RelayCommand addUser;
+        public RelayCommand AddUser
+        {
+            get
+            {
+                return addUser ??
+                    (addUser = new RelayCommand(obj =>
+                    {
+                        User newuser = new User() { ID = Guid.NewGuid() };
+                        Users.Insert(Users.Count, newuser);
+                        SelectedUser = newuser;
+                    }));
+            }
+        }
+
+        private void LoadFile()
+        {
+            
+            XmlSerializer xmlForamtter = new XmlSerializer(typeof(ObservableCollection<User>), rootAttribute);
+            using (FileStream fs = new FileStream("users.xml" , FileMode.OpenOrCreate))
+            {
+                Users = (ObservableCollection<User>)xmlForamtter.Deserialize(fs);
+            }
+        }
+
+        private void SaveFile()
+        {
+            XmlSerializer xmlFormatter = new XmlSerializer(typeof(ObservableCollection<User>), rootAttribute);
+            using (FileStream fs = new FileStream("users.xml", FileMode.OpenOrCreate))
+            {
+                xmlFormatter.Serialize(fs, Users);
+            }
+        }
+    }
+}
